04658837274d733b9894b3f1560c9ee02aca1cee
Emancus09 <send.emanuel@hotmail.com>
2018-01-28 02:34:32 -0500

    Add files via upload

--- Emanuel.js
+++ Emanuel.js
@@ -1,39 +1,14 @@
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const fs = require('fs');
-const util = require('util');
 
-const app = express();
+  const input = "take me to 12190 avenue henri beau from Concordia by metro"; //req.body.Body.str.toLowerCase();
 
-app.use(bodyParser());
+  const from = input.indexOf("from ");
+  const to = input.indexOf("to ");
+  const by = input.indexOf("by ");
 
-app.post('/sms', (req, res) => {
-
-  const twiml = new MessagingResponse();
-
-  const input = "take me from 12190 avenue henri beau to Concordia by metro" //req.body.Body.str.toLowerCase();
-
-  const from = input.indexOf(" from ");
-  const to = input.indexOf(" to ");
-  const by = input.indexOf(" by ");
-
-  const location = input.substring(from + 6, to);
-  const destination = input.substring(to + 4, by);
-  const method = input.substring(by + 4, input.length);
+  const location = input.substring(from + 5, Math.min((to < from) ? input.length : to, (by < from) ? input.length : by));
+  const destination = input.substring(to + 3, Math.min((from < to) ? input.length : from, (by < to) ? input.length : by));
+  const method = input.substring(by + 3, Math.min((from < by) ? input.length : from, (to < by) ? input.length : to));
 
   console.log("location : " + location);
   console.log("destination : " + destination);
   console.log("method : " + method);
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-  
-  console.log(req.body.Body);
-
-});
-
-http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
-});
