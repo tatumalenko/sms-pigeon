33956f6b8d5e83e04807ff5526b47892cc3e12c9
Tatum Alenko <tatum.alenko@gmail.com>
2018-01-28 05:53:39 -0500

    IT WORKS!

--- Emanuel.js
+++ Emanuel.js
@@ -1,14 +1,13 @@
+const input = 'take me to 3423 avenue henri beau by metro from 32423 concordia'; // req.body.Body.str.toLowerCase();
 
-  const input = "take me to 12190 avenue henri beau from Concordia by metro"; //req.body.Body.str.toLowerCase();
+const from = input.indexOf('from ');
+const to = input.indexOf('to ');
+const by = input.indexOf('by ');
 
-  const from = input.indexOf("from ");
-  const to = input.indexOf("to ");
-  const by = input.indexOf("by ");
+const location = (from != -1) ? input.substring(from + 5, Math.min((to < from) ? input.length : to, (by < from) ? input.length : by)) : '';
+const destination = (to != -1) ? input.substring(to + 3, Math.min((from < to) ? input.length : from, (by < to) ? input.length : by)) : '';
+const method = (by != -1) ? input.substring(by + 3, Math.min((from < by) ? input.length : from, (to < by) ? input.length : to)) : '';
 
-  const location = input.substring(from + 5, Math.min((to < from) ? input.length : to, (by < from) ? input.length : by));
-  const destination = input.substring(to + 3, Math.min((from < to) ? input.length : from, (by < to) ? input.length : by));
-  const method = input.substring(by + 3, Math.min((from < by) ? input.length : from, (to < by) ? input.length : to));
-
-  console.log("location : " + location);
-  console.log("destination : " + destination);
-  console.log("method : " + method);
+console.log(`location : ${location}`);
+console.log(`destination : ${destination}`);
+console.log(`method : ${method}`);
